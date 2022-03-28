f8fe42c1539b7594f823377885f389554b41faba
JohanGJensen <johancentralen@gmail.com>
2022-03-28 21:38:31 +0200

    updates types

--- src/interfaces/providers.ts
+++ src/interfaces/providers.ts
@@ -3,9 +3,9 @@ import { ISpace } from ".";
 export interface IState {
   data: ISpace[];
   loading: boolean;
-  handleDeleteItem: (space: ISpace, item: any) => void;
-  handleDeleteSpace: (spaceId: string) => void;
-  handleAddItem: (id: string, item: any) => void;
-  handleAddSpace: (space: ISpace) => void;
-  handleUpdateItem: (id: string, item: any) => void;
+  handleDeleteItem(): void;
+  handleDeleteSpace(): void;
+  handleAddItem(): void;
+  handleAddSpace(): void;
+  handleUpdateItem(): void;
 }
