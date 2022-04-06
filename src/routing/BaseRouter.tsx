2ca3362bde1f060b08131c5079b14299cfb0f537
JohanGJensen <johancentralen@gmail.com>
2022-04-06 21:17:46 +0200

    lazy load views

--- src/routing/BaseRouter.tsx
+++ src/routing/BaseRouter.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from "react-router-dom";
 
 // components
-import HomePage from '../views/HomePage';
-import SettingsPage from '../views/SettingsPage';
+import { LoadingOverlay } from '@mantine/core';
+const HomePage = lazy(() => import('../views/HomePage'));
+const SettingsPage = lazy(() => import('../views/SettingsPage'));
 
 interface IProps { }
 
 const BaseRouter: React.FC<IProps> = () => {
 
   return (
-    <Routes>
-      <Route
-        path={'/'}
-        element={<HomePage />}
-      />
-      <Route
-        path={'/settings'}
-        element={<SettingsPage />}
-      />
-    </Routes>
+    <Suspense fallback={<LoadingOverlay visible={true} />}>
+      <Routes>
+        <Route
+          path={'/'}
+          element={<HomePage />}
+        />
+        <Route
+          path={'/settings'}
+          element={<SettingsPage />}
+        />
+      </Routes>
+    </Suspense>
   );
 }
 
