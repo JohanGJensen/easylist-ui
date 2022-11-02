3536a9b8283caf4543c1019c7562490c35100382
JohanGJensen <johancentralen@gmail.com>
2022-11-02 21:25:34 +0100

    remove module preload from build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/easylist-ui-pwa/',
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   build: {
     outDir: 'build',
+    modulePreload: false,
   },
   server: {
     open: true,
