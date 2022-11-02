814df675d42caf9e4a81e634f1e8a5d164c81459
JohanGJensen <johancentralen@gmail.com>
2022-11-02 21:16:35 +0100

    remove plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
-import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/easylist-ui-pwa/',
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react()],
   build: {
     outDir: 'build',
-    rollupOptions: {
-      external: [],
-      output: {
-        plugins: [
-          getBabelOutputPlugin({
-            allowAllFormats: true,
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: '> 0.25%, not dead, IE 11',
-                  useBuiltIns: false,
-                  modules: false
-                },
-              ]
-            ]
-          }),
-        ]
-      },
-    },
   },
   server: {
     open: true,
