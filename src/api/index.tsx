5ce468e74d7cd71bacfe5df6db0427d87f933b05
JohanGJensen <johancentralen@gmail.com>
2023-04-02 22:19:54 +0200

    add connection endpoint

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,32 +1,44 @@
 import axios from 'axios';
+import { ConnectionStatusResponse } from 'interfaces/connectionStatus';
 import { IItemRequest, ISpaceRequest } from '../interfaces';
 import { IRequestMutation } from './mutations/useMutateItems';
 
 const HOST = import.meta.env.VITE_API_HOST;
 
+export const http = axios.create({
+  baseURL: HOST,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  },
+});
+
+export const getStatus = () => {
+  return http.get<ConnectionStatusResponse>(`/health`);
+};
+
 export const getAllSpaces = () => {
-  return axios.get(`${HOST}/spaces/all`);
+  return http.get(`${HOST}/spaces/all`);
 };
 
 export const postNewSpace = (request: ISpaceRequest) => {
-  return axios.post(`${HOST}/spaces/create`, request);
+  return http.post(`${HOST}/spaces/create`, request);
 };
 
 export const deleteSpace = (spaceId: string) => {
-  return axios.delete(`${HOST}/spaces/delete/${spaceId}`);
+  return http.delete(`${HOST}/spaces/delete/${spaceId}`);
 };
 
 export const postNewItem = (data: IRequestMutation<IItemRequest>) => {
   const { spaceId, request } = data;
-  return axios.post(`${HOST}/items/create/${spaceId}`, request);
+  return http.post(`${HOST}/items/create/${spaceId}`, request);
 };
 
 export const postItemUpdate = (data: IRequestMutation<IItemRequest>) => {
   const { spaceId, itemId, request } = data;
-  return axios.post(`${HOST}/items/update/${spaceId}/${itemId}`, request);
+  return http.post(`${HOST}/items/update/${spaceId}/${itemId}`, request);
 };
 
 export const deleteItem = (data: IRequestMutation<IItemRequest>) => {
   const { spaceId, itemId } = data;
-  return axios.delete(`${HOST}/items/delete/${spaceId}/${itemId}`);
+  return http.delete(`${HOST}/items/delete/${spaceId}/${itemId}`);
 };
