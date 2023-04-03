bdb8c5e5ad1e68208517585dfbf0cdf25813e416
JohanGJensen <johancentralen@gmail.com>
2023-04-03 15:29:29 +0200

    added local storage caching while not connected to server

--- src/components/BackendStatus/BackendStatus.tsx
+++ src/components/BackendStatus/BackendStatus.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { Box, Loader, Text } from '@mantine/core';
+import { Box, ThemeIcon } from '@mantine/core';
 import { useCheckConnection } from 'api/queries/useCheckConnection';
-
-const statusColors = {
-  notOnline: {
-    primary: '#FFFFFF',
-    secondary: '#F24B4B',
-  },
-  online: {
-    secondary: '#5A834A',
-    primary: '#FFFFFF',
-  },
-};
+import { PlugConnected, PlugConnectedX } from 'tabler-icons-react';
 
 const BackendStatus: React.FC = () => {
-  const { online, notOnline } = statusColors;
-
-  const { online: isOnline } = useCheckConnection();
+  const { isOnline } = useCheckConnection();
 
   return (
     <Box
@@ -24,20 +12,11 @@ const BackendStatus: React.FC = () => {
         position: 'fixed',
         bottom: '1em',
         left: '1em',
-        backgroundColor: isOnline ? online.secondary : notOnline.secondary,
-        padding: '0.35em',
-        borderRadius: '0.35em',
-        color: isOnline ? online.primary : notOnline.primary,
       })}
     >
-      <Text
-        style={{ display: 'flex', alignItems: 'center', gap: '0.25em' }}
-        weight={700}
-        size={'sm'}
-      >
-        {isOnline ? 'connected' : 'not connected'}
-        {!isOnline && <Loader color={notOnline.primary} size={'xs'} />}
-      </Text>
+      <ThemeIcon color={isOnline ? 'green' : 'red'} radius="xl" size="xl">
+        {isOnline ? <PlugConnected /> : <PlugConnectedX />}
+      </ThemeIcon>
     </Box>
   );
 };
